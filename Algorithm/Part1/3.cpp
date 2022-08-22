c0e962ab61d24b17eee619f0c96c4638009020c0
online5880 <gjtjqkr1@naver.com>
2022-08-22 23:46:25 +0900

    4. 나이차이

--- Algorithm/Part1/3.cpp
+++ Algorithm/Part1/3.cpp
@@ -1,22 +1,22 @@
-// 3. 진약수의 합
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-
-	int n, sum = 1, i = 0;
-
-	cin >> n;
-	cout << "1";
-	for (i = 2; i < n; i++) {
-		if (n % i == 0) {
-			cout << "+" << i;
-			sum += i;
-		}
-	}
-	cout << "=" << sum;
-
-	return 0;
-}
+//// 3. 진약수의 합
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int n, sum = 1, i = 0;
+//
+//	cin >> n;
+//	cout << "1";
+//	for (i = 2; i < n; i++) {
+//		if (n % i == 0) {
+//			cout << "+" << i;
+//			sum += i;
+//		}
+//	}
+//	cout << "=" << sum;
+//
+//	return 0;
+//}
